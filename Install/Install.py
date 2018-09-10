5ad8dde404df57fb4c2302fd5b2548d2f419c5f7
BjornNyberg <37108459+BjornNyberg@users.noreply.github.com>
2018-09-10 16:48:47 +0200

    Install Update
    Added version control

--- Install/Install.py
+++ Install/Install.py
@@ -27,7 +27,7 @@ folder = dirname[1].replace('x64','')
 
 python_exe = os.path.join(dirname[0],folder,'python.exe')
 
-modules = ['pip','python-ternary','scipy','pandas','networkx==1.8','xlsxwriter']
+modules = ['pip','python-ternary==1.0.4','scipy==1.0.1','pandas==0.23.3','networkx==1.8','xlsxwriter==1.0.4','numpy==1.14.2']
 
 for module in modules:
     try:
